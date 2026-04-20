ae97859ddf29b5e2a95624eff2a3f19ecfaa2240
Arthur-Duarte <arthurfortniteplays@gmail.com>
2026-04-20 01:13:32 -0300

    Delete Python/Desafios GPT directory

--- Python/Desafios GPT/e4 (media_notas).py
+++
@@ -1,9 +0,0 @@
-n1 = float(input('Digite a primeira nota: '))
-n2 = float(input('Digite a segunda nota: '))
-media = (n1 + n2) / 2
-if media >= 7:
-    print(f'A média é {media:.2f}. Aluno aprovado!')
-elif media >= 5:
-    print(f'A média é {media:.2f}, Aluno em recuperação.')
-else: 
-    print(f'A média é {media:.2f}. Aluno reprovado!')
